75e5c41db10f6d857d7a4e8830ca48b46c86f591
Klyde-Tomasian <klydetomasian@outlook.com>
2021-05-25 14:07:35 -0700

    add let, const, var practices

--- myResources/JavaScript_Studies_fcc.js
+++ myResources/JavaScript_Studies_fcc.js
@@ -893,3 +893,33 @@
 //     : [...rangeOfNumbers(startNum, endNum - 1), endNum ];
 // }
 
+// ********************** ES6 *************************
+// When you declare a variable with the VAR keyword, it is declared
+// globally, or locally if declared inside a function.
+
+// The LET keyword behaves similarly, but with some extra features.
+// When you declare a variable with the let keyword inside a block,
+// statement, or expression, its scope is limited to that block,
+// statement, or expression.
+
+// CONST has all the awesome features that let has, with the added
+// bonus that variables declared using const are read-only.They are
+// a constant value, which means that once a variable is assigned
+// with const, it cannot be reassigned.
+// A common practice when naming constants is to use all uppercase letters,
+// with words separated by an underscore. ex: const FAV_PET = "Cats"
+// However, it is important to understand that objects(including arrays and
+// functions) assigned to a variable using const are still mutable.Using the
+// const declaration only prevents reassignment of the variable identifier.
+
+// MUTATE AN ARRAY DECLARED WITH CONST
+// const s = [5, 7, 2];
+// function editInPlace() {
+//   "use strict";
+//   s[0] = 2;
+//   s[1] = 5;
+//   s[2] = 7;
+// }
+// editInPlace();
+
+// PREVENT OBJECT MUTATION
