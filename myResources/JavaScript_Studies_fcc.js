93421dbfd2f0f621c56adbeb4b45e0b6c7bbbbd6
Klyde-Tomasian <klydetomasian@outlook.com>
2021-06-03 18:54:42 -0700

    add destructuring to assign variables from arrays

--- myResources/JavaScript_Studies_fcc.js
+++ myResources/JavaScript_Studies_fcc.js
@@ -1046,17 +1046,22 @@
 // const { today: { low: lowToday, high: highToday}} = LOCAL_FORECAST
 // console.log(lowToday, highToday)
 
-// USE DESTRUCTURING ASSIGNMENT TO ASSIGN VARIABLES FROM ARRAYS
-// One key difference between the spread operator and array destructuring is that
-// the spread operator unpacks all contents of an array into a comma - separated
-// list.Consequently, you cannot pick or choose which elements you want to assign
-// to variables.
-// Destructuring an array lets us do exactly that:
-const [a, b] = [1, 2, 3, 4, 5, 6];
-console.log(a, b);
-// The console will display the values of a and b as 1, 2.
-// The variable a is assigned the first value of the array, and b is assigned the
-// second value of the array.We can also access the value at any index in an array
-// with destructuring by using commas to reach the desired index:
-const [a, b,,, c] = [1, 2, 3, 4, 5, 6];
-console.log(a, b, c);
+// // // USE DESTRUCTURING ASSIGNMENT TO ASSIGN VARIABLES FROM ARRAYS
+// // // One key difference between the spread operator and array destructuring is that
+// // // the spread operator unpacks all contents of an array into a comma - separated
+// // // list.Consequently, you cannot pick or choose which elements you want to assign
+// // // to variables.
+// // Destructuring an array lets us do exactly that:
+// // const [a, b] = [1, 2, 3, 4, 5, 6];
+// // console.log(a, b);
+// // // The console will display the values of a and b as 1, 2.
+// // // The variable a is assigned the first value of the array, and b is assigned the
+// // // second value of the array.We can also access the value at any index in an array
+// // // with destructuring by using commas to reach the desired index:
+// // const [a, b,,, c] = [1, 2, 3, 4, 5, 6];
+// // console.log(a, b, c);
+// let a = 8, b = 6;
+// [a, b] = [b, a]
+// console.log(a, b)
+
+// USE DESTRUCTURING ASSIGNMENT WITH THE REST PARAMETER TO REASSIGN ARRAY ELEMENTS
