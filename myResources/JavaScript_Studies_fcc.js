f3fbbb0bed10c1649608401bbd03b2fcc00ee2f6
Klyde-Tomasian <klydetomasian@outlook.com>
2021-06-05 15:38:15 -0700

    add strings using template literals

--- myResources/JavaScript_Studies_fcc.js
+++ myResources/JavaScript_Studies_fcc.js
@@ -1044,7 +1044,7 @@
 
 // // // USE DESTRUCTURING ASSIGNMENT TO ASSIGN VARIABLES FROM ARRAYS
 // // // One key difference between the spread operator and array destructuring is that the spread operator unpacks all contents of an array into a comma - separated list.Consequently, you cannot pick or choose which elements you want to assign to variables.
-// // Destructuring an array lets us do exactly that:
+// // // Destructuring an array lets us do exactly that:
 // // const [a, b] = [1, 2, 3, 4, 5, 6];
 // // console.log(a, b);
 // // // The console will display the values of a and b as 1, 2.
@@ -1065,3 +1065,70 @@
 // const arr = removeFirstTwo(source);
 // console.log(source, arr)
 
+// // // USE DESTRUCTURING ASSIGNMENT TO PASS AN OBJECT AS A FUNCTION'S PARAMETERS
+// // const profileUpdate = (profileData) => {
+// //   const { name, age, nationality, location } = profileData;
+// // }
+// // // This effectively destructures the object sent into the function. This can also be done in-place:
+// // const profileUpdate = ({ name, age, nationality, location }) => {
+// // }
+// // // When profileData is passed to the above function, the values are destructured from the function parameter for use within the function.
+// const stats = {
+//   max: 56.78,
+//   standard_deviation: 4.34,
+//   median: 34.54,
+//   mode: 23.87,
+//   min: -0.75,
+//   average: 35.85
+// };
+// const half = ({max, min}) => (max + min) / 2.0;
+
+// // // CREATE STRINGS USING TEMPLATE LITERALS
+// // // Template literals allow you to create multi-line strings and to use string interpolation features to create strings.
+// // // Consider the code below:
+// // const person = {
+// //   name: "Zodiac Hasbro",
+// //   age: 56
+// // };
+// // const greeting = `Hello, my name is ${person.name}!
+// // I am ${person.age} years old.`;
+// // console.log(greeting);
+// // // The console will display the strings Hello, my name is Zodiac Hasbro! and I am 56 years old..
+// // // A lot of things happened there. Firstly, the example uses backticks (`), not quotes (' or "), to wrap the string. Secondly, notice that the string is multi-line, both in the code and the output. This saves inserting \n within strings. The ${variable} syntax used above is a placeholder. Basically, you won't have to use concatenation with the + operator anymore. To add variables to strings, you just drop the variable in a template string and wrap it with ${ and }. Similarly, you can include other expressions in your string literal, for example ${a + b}. This new way of creating strings gives you more flexibility to create robust strings.
+// // CHALLENGE PROBLEM
+// // Use template literal syntax with backticks to create an array of list element (li) strings. Each list element's text should be one of the array elements from the failure property on the result object and have a class attribute with the value text-warning. The makeList function should return the array of list item strings.
+// // Use an iterator method (any kind of loop) to get the desired output (shown below).
+// // [
+// //   '<li class="text-warning">no-var</li>',
+// //   '<li class="text-warning">var-on-top</li>',
+// //   '<li class="text-warning">linebreak</li>'
+// // ]
+// // SOLUTION 1:
+// const result = {
+//   success: ["max-length", "no-amd", "prefer-arrow-functions"],
+//   failure: ["no-var", "var-on-top", "linebreak"],
+//   skipped: ["no-extra-semi", "no-dup-keys"]
+// };
+// function makeList(arr) {
+//   "use strict";
+//   const failureItems = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     failureItems.push(`<li class="text-warning">${arr[i]}</li>`);
+//   }
+//   return failureItems;
+// }
+// const failuresList = makeList(result.failure);
+// console.log(failuresList)
+// // SOLUTION 2:
+// const result = {
+//   success: ["max-length", "no-amd", "prefer-arrow-functions"],
+//   failure: ["no-var", "var-on-top", "linebreak"],
+//   skipped: ["no-extra-semi", "no-dup-keys"]
+// };
+// function makeList(arr) {
+//   "use strict";
+//   const failureItems = arr.map(item => `<li class="text-warning">${item}</li>`);
+//   return failureItems;
+// }
+// const failuresList = makeList(result.failure);
+// console.log(failuresList)
