fa4d9da794770caea25f54b4aef806fc25b2991b
Klyde-Tomasian <klydetomasian@outlook.com>
2021-06-08 23:08:09 -0700

    add object preperty shorthand

--- myResources/JavaScript_Studies_fcc.js
+++ myResources/JavaScript_Studies_fcc.js
@@ -1132,3 +1132,16 @@
 // }
 // const failuresList = makeList(result.failure);
 // console.log(failuresList)
+
+// // WRITE CONCISE OBJECT LITERAL DECLARATIONS USING OBJECT PROPERTY SHORTHAND
+// // const getMousePosition = (x, y) => ({
+// //   x: x,
+// //   y: y
+// // });
+// // getMousePosition is a simple function that returns an object containing two properties. ES6 provides the syntactic sugar to eliminate the redundancy of having to write x: x. You can simply write x once, and it will be converted tox: x (or something equivalent) under the hood. Here is the same function from above rewritten to use this new syntax:
+// // const getMousePosition = (x, y) => ({ x, y });
+// const createPerson = (name, age, gender) => {
+//   return {name, age, gender};
+// };
+
+
